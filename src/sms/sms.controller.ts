96147fd99d037d32cb21e917e423d4b6fe233731
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-01-23 10:41:49 +0100

    updates

--- src/sms/sms.controller.ts
+++ src/sms/sms.controller.ts
@@ -1,31 +1,33 @@
 /* eslint-disable prettier/prettier */
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { GrpcMethod, Payload } from '@nestjs/microservices';
 import { SmsService } from './sms.service';
 import { CreateSmDto } from './dto/create-sm.dto';
 import { SendSMSDTO } from './dto/send-sms.dto';
-import { SaveSettingsDTO } from './dto/sms-settings.dto';
 import { SaveSettingsRequest, SaveSettingsResponse, SendSmsRequest, SendSmsResponse } from 'src/noti.pb';
 
 @Controller()
 export class SmsController {
   constructor(private readonly smsService: SmsService) { }
 
-  @MessagePattern('createSm')
+  @GrpcMethod('NotificationService', 'CreateSms')
   create(@Payload() createSmDto: CreateSmDto) {
     return this.smsService.create(createSmDto);
   }
-  @MessagePattern('sendSms')
-  async sendSms(@Payload() smsRequest: SendSmsRequest): Promise<SendSmsResponse> {
+
+  @GrpcMethod('NotificationService', 'SendSms')
+  async SendSms(smsRequest: SendSmsRequest): Promise<SendSmsResponse> {
+    console.log('Sending SMS')
     return this.smsService.handleSms(smsRequest);
   }
-  @MessagePattern('saveSettings')
-  async saveSettings(@Payload() saveSettings: SaveSettingsRequest): Promise<SaveSettingsResponse> {
+
+  @GrpcMethod('NotificationService', 'SaveSettings')
+  async SaveSettings(saveSettings: SaveSettingsRequest): Promise<SaveSettingsResponse> {
     return this.smsService.saveSettings(saveSettings);
   }
 
-  @MessagePattern('deliveryReport')
-  async deliveryReport(@Payload() deliveryReport: SendSMSDTO) {
+  @GrpcMethod('NotificationService', 'DeliveryReport')
+  async DeliveryReport(deliveryReport: SendSMSDTO) {
     return this.smsService.getDeliveryReport(deliveryReport);
   }
 }
