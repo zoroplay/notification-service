fc1faf431ca3bac185ec0e865d4036c66ac60ddd
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-03-17 23:16:17 +0100

    update debugging

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -226,7 +226,7 @@ export class SmsService {
             },
           },
         )
-      console.log(response);
+      console.log(response.data);
       if (response.data.status === false) {
         messageData.status = false;
         // save message as failed
